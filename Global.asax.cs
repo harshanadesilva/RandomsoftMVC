60a0936f083d83dee8642f1e1b66ed3ec5eddf03
harshanadesilva <79626939+harshanadesilva@users.noreply.github.com>
2021-02-26 09:21:43 +0530

    Add files via upload

--- Global.asax.cs
+++ Global.asax.cs
@@ -16,6 +16,21 @@ namespace MytestApp
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+
+            TimeSpan ShiftStart = new TimeSpan(9, 30, 0);
+            TimeSpan ShiftEnd = new TimeSpan(22, 0, 0);
+            var src = DateTime.Now;
+            TimeSpan timenow = new TimeSpan(src.Hour, src.Minute, 0);
+
+            if ((ShiftStart > ShiftEnd && (timenow > ShiftStart || timenow < ShiftEnd)) || (timenow > ShiftStart && timenow < ShiftEnd))
+            {
+                Console.Write("Yes You are in shift!");
+            }
+            else
+            {
+                Console.Write("Out of shift!");
+            }
         }
     }
 }
